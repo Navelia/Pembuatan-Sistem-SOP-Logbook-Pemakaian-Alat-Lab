1228a1cee139ca98906ecba16c09e0ced21caca5
Navellia <ncent4lyfe@gmail.com>
2023-06-23 16:00:25 +0700

    mahasiswa update

---
+++ kpftbio/resources/views/mahasiswa/alat_detail.blade.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+    </script>
+
+    <style>
+        html,
+        body {
+            height: 100%;
+        }
+
+        body {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+        }
+
+        .container {
+            text-align: center;
+            background: rgb(250, 205, 205);
+            padding: 25px;
+        }
+
+        .image-container {
+            display: block;
+            margin-left: auto;
+            margin-right: auto;
+            width: 500px;
+        }
+
+        .paragraphs-container {
+            margin-top: 50px;
+            display: flex;
+            justify-content: space-around;
+        }
+    </style>
+</head>
+
+
+<body>
+    <div class="container">
+        <div class="image-container">
+            <img src="{{ asset('images/' . $data->gambar) }}" class="card-img-top img-fluid">
+        </div>
+        <div class="paragraphs-container">
+            <h1>{{ $data->nama }}</h1>
+        </div>
+        <p>{{ $data->deskripsi }}</p>
+        <div class="d-grid gap-2">
+            <a href="#" class="btn btn-primary">Pinjam Alat</a>
+            <a href="#" class="btn btn-primary">Riwayat Peminjaman</a>
+        </div>
+
+        @foreach ($data->spesifikasi as $spek)
+            <tr>
+                <td>
+                    <p>{{ $spek->nama_spesifikasi }}</p>
+                </td>
+                <td>
+                    <p>{{ $spek->spesifikasi }}</p>
+                </td>
+            </tr>
+        @endforeach
+    </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+
+</html>
--- kpftbio/resources/views/mahasiswa/welcome.blade.php
+++ kpftbio/resources/views/mahasiswa/welcome.blade.php
@@ -1,2 +1,69 @@
-@extends('layouts.conquer')
-@section('content')
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+    </script>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <div class="container">
+                <a class="navbar-brand" href="#">
+                    <img src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="Bootstrap" width="30" height="24">
+                </a>
+            </div>
+            {{-- <a class="navbar-brand" href="#">Logo</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button> --}}
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                </ul>
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
+        </div>
+    </nav>
+
+
+    <div class="container">
+        <div class="row">
+            @foreach ($data as $d)
+                <div class="col-md-3">
+                    <div class="card">
+                        <img src="{{ asset('images/' . $d->gambar) }}" class="card-img-top" alt="...">
+
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $d->nama }}</h5>
+                            <p class="card-text">{{ $d->deskripsi }}</p>
+                            <div class="d-grid gap-2">
+                                <a href="{{ url('detailAlat/' . $d->id) }}" class="btn btn-primary">Detail Alat</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
+</body>
+
+</html>
