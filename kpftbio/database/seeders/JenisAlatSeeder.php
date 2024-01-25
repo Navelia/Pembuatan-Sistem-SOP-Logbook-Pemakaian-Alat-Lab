8996ea5f99e6bc1dfc60ae5e2524055e3a8732ab
Navellia <ncent4lyfe@gmail.com>
2024-01-26 02:16:43 +0700

    seeder alat

--- kpftbio/database/seeders/JenisAlatSeeder.php
+++ kpftbio/database/seeders/JenisAlatSeeder.php
@@ -15,15 +15,207 @@ class JenisAlatSeeder extends Seeder
     public function run()
     {
         DB::table("jenis_alats")->insert([
-            "nama" => "Jenis Alat 1",
-            "deskripsi" => "Ini Deskripsi Jenis Alat 1",
-            "gambar" => "jenis-1.jpg"
+            "nama" => "Electroporator",
+            "deskripsi" => "-",
+            "gambar" => "ELECTROPRORATOR.jpg"
         ]);
 
         DB::table("jenis_alats")->insert([
-            "nama" => "Jenis Alat 2",
-            "deskripsi" => "Ini Deskripsi Jenis Alat 2",
-            "gambar" => "jenis-2.jpg"
+            "nama" => "Fluorescence Microscope",
+            "deskripsi" => "-",
+            "gambar" => "FLUORESCENCE MICROSCOPE.jpg"
+        ]);
+
+        DB::table("jenis_alats")->insert([
+            "nama" => "Gel-Doc",
+            "deskripsi" => "-",
+            "gambar" => "GEL-DOC.jpg"
+        ]);
+
+        DB::table("jenis_alats")->insert([
+            "nama" => "Horizontal Electroforesis",
+            "deskripsi" => "-",
+            "gambar" => "HORIZONTAL ELECTROFORESIS.jpg"
+        ]);
+
+        DB::table("jenis_alats")->insert([
+            "nama" => "HPLC",
+            "deskripsi" => "-",
+            "gambar" => "HPLC.jpg"
+        ]);
+
+        DB::table("jenis_alats")->insert([
+            "nama" => "Inkubator Shaker",
+            "deskripsi" => "-",
+            "gambar" => "INKUBATOR SHAKER.jpg"
+        ]);
+
+        DB::table("jenis_alats")->insert([
+            "nama" => "Kompor Listrik",
+            "deskripsi" => "-",
+            "gambar" => "KOMPOR LISTRIK.jpg"
+        ]);
+
+        DB::table("jenis_alats")->insert([
+            "nama" => "Laminair Air Flow",
+            "deskripsi" => "-",
+            "gambar" => "LAMINAIR AIR FLOW.jpg"
+        ]);
+
+        DB::table("jenis_alats")->insert([
+            "nama" => "Lemari Asam",
+            "deskripsi" => "-",
+            "gambar" => "LEMARI ASAM.jpg"
+        ]);
+
+        DB::table("jenis_alats")->insert([
+            "nama" => "Microcentrifuse Dingin",
+            "deskripsi" => "-",
+            "gambar" => "MICROCENTRIFUSE DINGIN.jpg"
+        ]);
+
+        DB::table("jenis_alats")->insert([
+            "nama" => "Microplate Reader",
+            "deskripsi" => "-",
+            "gambar" => "MICROPLATE READER.jpg"
+        ]);
+
+        DB::table("jenis_alats")->insert([
+            "nama" => "Microvac",
+            "deskripsi" => "-",
+            "gambar" => "MICROVAC.jpg"
+        ]);
+
+        DB::table("jenis_alats")->insert([
+            "nama" => "Mikroskop Kamera",
+            "deskripsi" => "-",
+            "gambar" => "MIKROSKOP KAMERA.jpg"
+        ]);
+
+        DB::table("jenis_alats")->insert([
+            "nama" => "Mini PCR",
+            "deskripsi" => "-",
+            "gambar" => "MINI PCR.jpg"
+        ]);
+
+        DB::table("jenis_alats")->insert([
+            "nama" => "Otoklaf",
+            "deskripsi" => "-",
+            "gambar" => "OTOKLAF.jpg"
+        ]);
+
+        DB::table("jenis_alats")->insert([
+            "nama" => "Oven Microwave",
+            "deskripsi" => "-",
+            "gambar" => "OVEN MICROWAVE.jpg"
+        ]);
+
+        DB::table("jenis_alats")->insert([
+            "nama" => "Oven Yaneco",
+            "deskripsi" => "-",
+            "gambar" => "OVEN YANECO.jpg"
+        ]);
+
+        DB::table("jenis_alats")->insert([
+            "nama" => "PCR Bio-Rad",
+            "deskripsi" => "-",
+            "gambar" => "PCR BIO-RAD.jpg"
+        ]);
+
+        DB::table("jenis_alats")->insert([
+            "nama" => "PCR Gradient",
+            "deskripsi" => "-",
+            "gambar" => "PCR GRADIENT.jpg"
+        ]);
+
+        DB::table("jenis_alats")->insert([
+            "nama" => "pH Meter",
+            "deskripsi" => "-",
+            "gambar" => "pH METER.jpg"
+        ]);
+
+        DB::table("jenis_alats")->insert([
+            "nama" => "Power Supply Hoefer",
+            "deskripsi" => "-",
+            "gambar" => "POWER SUPPLY HOEFER.jpg"
+        ]);
+
+        DB::table("jenis_alats")->insert([
+            "nama" => "Rotary Evaporator",
+            "deskripsi" => "-",
+            "gambar" => "ROTARY EVAPORATOR.jpg"
+        ]);
+
+        DB::table("jenis_alats")->insert([
+            "nama" => "RT-PCR BIO-RAD",
+            "deskripsi" => "-",
+            "gambar" => "RT-PCR BIO-RAD.jpg"
+        ]);
+
+        DB::table("jenis_alats")->insert([
+            "nama" => "RT-PCR MY-GO",
+            "deskripsi" => "-",
+            "gambar" => "RT-PCR MY-GO.jpg"
+        ]);
+
+        DB::table("jenis_alats")->insert([
+            "nama" => "Sentrifuse 50 mL",
+            "deskripsi" => "-",
+            "gambar" => "SENTRIFUSE 50 ML.jpg"
+        ]);
+
+        DB::table("jenis_alats")->insert([
+            "nama" => "Shaker Resiprocal",
+            "deskripsi" => "-",
+            "gambar" => "SHAKER RESIPROCAL.jpg"
+        ]);
+
+        DB::table("jenis_alats")->insert([
+            "nama" => "Spektrofotometer",
+            "deskripsi" => "-",
+            "gambar" => "SPEKTROFOTOMETER.jpg"
+        ]);
+
+        DB::table("jenis_alats")->insert([
+            "nama" => "Spindown",
+            "deskripsi" => "-",
+            "gambar" => "SPINDOWN.jpg"
+        ]);
+
+        DB::table("jenis_alats")->insert([
+            "nama" => "Timbangan Digital",
+            "deskripsi" => "-",
+            "gambar" => "TIMBANGAN DIGITAL.jpg"
+        ]);
+
+        DB::table("jenis_alats")->insert([
+            "nama" => "Ultrasonic Cleaner",
+            "deskripsi" => "-",
+            "gambar" => "ULTRASONIC CLEANER.jpg"
+        ]);
+
+        DB::table("jenis_alats")->insert([
+            "nama" => "Ultrasonic Destruptor",
+            "deskripsi" => "-",
+            "gambar" => "ULTRASONIC DESTRUPTOR.jpg"
+        ]);
+
+        DB::table("jenis_alats")->insert([
+            "nama" => "UV Viewing Cabinet",
+            "deskripsi" => "-",
+            "gambar" => "UV VIEWING CABINET.jpg"
+        ]);
+
+        DB::table("jenis_alats")->insert([
+            "nama" => "Vortex",
+            "deskripsi" => "-",
+            "gambar" => "VORTEX.jpg"
+        ]);
+
+        DB::table("jenis_alats")->insert([
+            "nama" => "Waterbath Faithfull",
+            "deskripsi" => "-",
+            "gambar" => "WATERBATH FAITHFULL.jpg"
         ]);
     }
 }
